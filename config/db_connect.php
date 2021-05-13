ce1dfa18cbdd2ea5f8f4a93db741c3c8e7a22302
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2021-05-14 02:05:59 +0800

    first commit

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,9 +1,18 @@
 <?php
 
-// connect to database
-$conn = mysqli_connect('localhost', 'ezzy', 'test1234', 'ez_pizza');
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
 
-// check connection
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 if (!$conn) echo 'Connection error: ' . mysqli_connect_error();
 
 ?>
